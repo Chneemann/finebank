e3c156fabc27c47e17295fc067aea9bc44a0c801
Chneemann <andre-kempf@mailbox.org>
2025-01-26 17:48:37 +0100

    feat: add logout button to Navbar and design Header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  today: Date = new Date();
 }
