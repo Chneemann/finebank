68b5d35be6add5edeb60d2f02b69681796d2c930
Chneemann <andre-kempf@mailbox.org>
2025-01-28 10:40:11 +0100

    feat: enable interactive slider navigation for paging through cards

--- src/app/features/balances/overview-card/slider/slider.component.ts
+++ src/app/features/balances/overview-card/slider/slider.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+  styleUrl: './slider.component.scss',
 })
 export class SliderComponent {
+  currentIndex = 0;
+  totalSlides = 3;
 
+  nextSlide() {
+    if (this.currentIndex < this.totalSlides - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  prevSlide() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
 }
