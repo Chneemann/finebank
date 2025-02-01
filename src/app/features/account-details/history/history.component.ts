f84290225eb916c5ba9705dedb40cdfed21a0e34
Chneemann <andre-kempf@mailbox.org>
2025-02-01 08:40:31 +0100

    refactor: extract transaction history section into a separate HistoryComponent for improved modularity

---
+++ src/app/features/account-details/history/history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/components/layouts/button/button.component';
+
+@Component({
+  selector: 'app-history',
+  imports: [ButtonComponent],
+  templateUrl: './history.component.html',
+  styleUrl: './history.component.scss',
+})
+export class HistoryComponent {}
