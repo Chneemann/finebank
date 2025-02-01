f2eba83253a090f9ecd19b598e5409d98f6aa47f
Chneemann <andre-kempf@mailbox.org>
2025-02-01 11:22:19 +0100

    feat: display data using date and currency pipes for formatted output

--- src/app/features/balances/components/overview-card/overview-card.component.ts
+++ src/app/features/balances/components/overview-card/overview-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SliderComponent } from './slider/slider.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-overview-card-balances',
-  imports: [SliderComponent],
+  imports: [CommonModule, SliderComponent],
   templateUrl: './overview-card.component.html',
   styleUrl: './overview-card.component.scss',
 })
