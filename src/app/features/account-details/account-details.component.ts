f9e04ae90fbbd4dde8e6c39efb86e0c21fefdb0f
Chneemann <andre-kempf@mailbox.org>
2025-02-01 08:37:46 +0100

    refactor: extract account-details into separate DetailsComponent for improved modularity

--- src/app/features/account-details/account-details.component.ts
+++ src/app/features/account-details/account-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ButtonComponent } from '../../shared/components/layouts/button/button.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
   selector: 'app-account-details',
-  imports: [ButtonComponent],
+  imports: [DetailsComponent, ButtonComponent],
   templateUrl: './account-details.component.html',
   styleUrl: './account-details.component.scss',
 })
---
+++ src/app/features/account-details/details/details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/components/layouts/button/button.component';
+
+@Component({
+  selector: 'app-details',
+  imports: [ButtonComponent],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.scss',
+})
+export class DetailsComponent {}
