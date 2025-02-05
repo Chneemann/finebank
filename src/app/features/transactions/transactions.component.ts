42da34f89489d711545fdf41d0240541040350d6
Chneemann <andre-kempf@mailbox.org>
2025-02-05 20:03:38 +0100

    feat: add and design navigation menu

--- src/app/features/transactions/transactions.component.ts
+++ src/app/features/transactions/transactions.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TransactionsService } from './services/transactions.service';
 import { ButtonComponent } from '../../shared/components/layouts/button/button.component';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-transactions',
-  imports: [ButtonComponent, CommonModule],
+  imports: [RouterLink, RouterModule, ButtonComponent, CommonModule],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss',
 })
