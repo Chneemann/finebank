c1490a1cd9aa7b4eb7bb03a11d327e3c964152da
Chneemann <andre-kempf@mailbox.org>
2025-02-06 20:15:48 +0100

    feat: create and integrate TransactionsCardComponent

--- src/app/features/overview/overview.component.ts
+++ src/app/features/overview/overview.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { OverviewCardBalancesComponent } from '../balances/components/overview-card/overview-card.component';
+import { TransactionsCardComponent } from '../transactions/components/transactions-card/transactions-card.component';
 
 @Component({
   selector: 'app-overview',
-  imports: [OverviewCardBalancesComponent],
+  imports: [OverviewCardBalancesComponent, TransactionsCardComponent],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss',
 })
---
+++ src/app/features/transactions/components/transactions-card/transactions-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions-card',
+  imports: [],
+  templateUrl: './transactions-card.component.html',
+  styleUrl: './transactions-card.component.scss'
+})
+export class TransactionsCardComponent {
+
+}
