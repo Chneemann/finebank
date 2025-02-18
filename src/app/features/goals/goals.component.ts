43e11a12d615b147e75d47df8c34516426ee79da
Chneemann <andre-kempf@mailbox.org>
2025-02-18 19:27:26 +0100

    feat: integrate GoalsService into GoalsComponent and display data

--- src/app/features/goals/goals.component.ts
+++ src/app/features/goals/goals.component.ts
@@ -4,6 +4,7 @@ import { ManometerComponent } from './manometer/manometer.component';
 import { CommonModule } from '@angular/common';
 import { Observable, Subject } from 'rxjs';
 import { BalancesService } from '../../core/services/balances.service';
+import { GoalsService } from '../../core/services/goals.service';
 
 @Component({
   selector: 'app-goals',
@@ -17,12 +18,17 @@ export class GoalsComponent {
   thisMonthTarget: number = 10000;
 
   private destroy$ = new Subject<void>();
+  allGoals$!: Observable<{ goal: string; amount: number }[]>;
   globalBalance$!: Observable<number>;
   accountsBalances$!: Observable<{ accountId: string; balance: number }[]>;
 
-  constructor(private balancesService: BalancesService) {}
+  constructor(
+    private balancesService: BalancesService,
+    private goalsService: GoalsService
+  ) {}
 
   ngOnInit(): void {
+    this.allGoals$ = this.goalsService.allGoals$;
     this.globalBalance$ = this.balancesService.globalBalance$;
     this.accountsBalances$ = this.balancesService.accountsBalances$;
   }
