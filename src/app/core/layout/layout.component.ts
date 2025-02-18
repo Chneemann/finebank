a6ab7f3997a844e9af3ea252411b8d56410635cf
Chneemann <andre-kempf@mailbox.org>
2025-02-18 20:56:46 +0100

    refactor: rename OverviewComponent to OverlayComponent and integrate it into LayoutComponent

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { OverlayComponent } from '../../shared/components/overlay/overlay.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent, OverlayComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
