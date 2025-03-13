7dd0c5d976df079d2309babed8d61ca81740a1ee
Chneemann <andre-kempf@mailbox.org>
2025-03-13 08:40:16 +0100

    feat: integrate MonthYearPickerComponent into BreakdownComponent for category breakdown selection

--- src/app/features/expenses/expenses.component.ts
+++ src/app/features/expenses/expenses.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { LineChartExpensesComponent } from './line-chart-expenses/line-chart-expenses.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SettingsService } from '../../core/services/settings.service';
 import { Observable, Subject, takeUntil } from 'rxjs';
 import { BreakdownComponent } from './breakdown/breakdown.component';
+import { MonthYearPickerComponent } from '../transactions/transactions-list/month-year-picker/month-year-picker.component';
 
 @Component({
   selector: 'app-expenses',
@@ -13,12 +14,16 @@ import { BreakdownComponent } from './breakdown/breakdown.component';
     FormsModule,
     LineChartExpensesComponent,
     BreakdownComponent,
+    MonthYearPickerComponent,
   ],
   templateUrl: './expenses.component.html',
   styleUrl: './expenses.component.scss',
 })
 export class ExpensesComponent {
   private destroy$ = new Subject<void>();
+
+  @ViewChild(MonthYearPickerComponent)
+  monthYearPickerComponent!: MonthYearPickerComponent;
 
   selectedYear: number = new Date().getFullYear();
   years: number[] = [2024, 2025];
@@ -54,5 +59,42 @@ export class ExpensesComponent {
       .subscribe({
         error: (err) => console.error('Error updating year:', err),
       });
+  }
+
+  getSettingsMonth(settings: any): number {
+    return settings?.selectedTransactionPeriod
+      ? parseInt(settings.selectedTransactionPeriod.slice(0, 2), 10)
+      : 1;
+  }
+
+  getSettingsMonthName(settings: any): string {
+    const month = settings?.selectedTransactionPeriod
+      ? parseInt(settings.selectedTransactionPeriod.slice(0, 2), 10)
+      : 1;
+    const monthNames = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    return monthNames[month - 1];
+  }
+
+  getSettingsYear(settings: any): number {
+    return settings?.selectedTransactionPeriod
+      ? parseInt(settings.selectedTransactionPeriod.slice(2, 6), 10)
+      : new Date().getFullYear();
+  }
+
+  toggleMonthYearPicker() {
+    this.monthYearPickerComponent.toggleDatePicker();
   }
 }
